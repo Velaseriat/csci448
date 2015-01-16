ce1e7b8397a7b7b0322d38ed22b26b3019bd4719
aaflack18 <kmasuda@mymail.mines.edu>
2015-01-16 00:34:45 -0700

    HW1

---
+++ hw1/Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by Kelly Masuda on 1/15/15.
+//  Copyright (c) 2015 Kelly Masuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
